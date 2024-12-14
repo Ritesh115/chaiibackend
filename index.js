748f84ebd9a29c6f8c741aa5cb5aef37d757e924
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-12-14 04:15:00 +0000

    commt

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/twitter', (req,res) => {
+   res.get('<h1>hello i am back</h1>')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
