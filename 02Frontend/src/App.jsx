88910c31507f1454f763cad664bbe5ce8efd4e26
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-12-16 05:06:18 +0000

    commit

--- 02Frontend/src/App.jsx
+++ 02Frontend/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [user_data, setuser_data] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/login")
+      .then((response) => {
+        setuser_data(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
       <h1>chai and fullstack</h1>
       <p>user_data strength : {user_data.length}</p>
-      {user_data.map((item) => {
+      {user_data.map((user_data) => {
         <div key={user_data.id}>
           <h1>{user_data.username}</h1>
           <p>{username.age}</p>
