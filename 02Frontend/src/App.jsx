8a1375445875ecfc63eb8b8484ae6105a9cd92c0
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-12-29 06:39:56 +0000

    commit

--- 02Frontend/src/App.jsx
+++ 02Frontend/src/App.jsx
@@ -7,25 +7,15 @@ import axios from "axios";
 function App() {
   const [user_data, setuser_data] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:5000/login")
-      .then((response) => {
-        setuser_data(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <>
       <h1>chai and fullstack</h1>
       <p>user_data strength : {user_data.length}</p>
-      {user_data.map((user_data) => {
-        <div key={user_data.id}>
-          <h1>{user_data.username}</h1>
-          <p>{username.age}</p>
+
+      {user_data.map((user) => {
+        <div key={"user.id"}>
+          <h3>{user.username}</h3>
+          <p> {user.email} </p>
         </div>;
       })}
     </>
