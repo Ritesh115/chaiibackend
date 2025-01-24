b841d561062b29f13d5533099790ec3f379931de
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2025-01-24 10:17:47 +0000

    commit

--- backend_project/src/db/index.js
+++ backend_project/src/db/index.js
@@ -1,2 +1,17 @@
+//connecting DB throygh mongoose
 import mongoose from "mongoose" ;
-import {DB_NAME} from "../constants" ;
+import {DB_NAME} from "../constants.js" ;
+
+
+const connectDB = async () => {
+      try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MONGODB connected SUCCESSFULLY !! DB HOST${connectionInstance.connection.host} \n`);
+      } catch (error) {
+        console.log("MONGODB connection FAILED " , error);
+        process.exit(1)
+      }
+}
+
+
+export default connectDB ;
