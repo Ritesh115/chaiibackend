6d3d6e0249227388d62433d90616dded207f9cf3
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2025-02-15 10:31:13 +0000

    commit

--- backend_project/src/app.js
+++ backend_project/src/app.js
@@ -1,40 +1,35 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+const app = express();
 
-const app = express(); 
-
-app.use( cors(
-  {
-    origin : process.env.CORS_ORIGIN,  // all the requests from this origin will be allowed.
-    credentials : true
-  }
-) )
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // all the requests from this origin will be allowed.
+    credentials: true,
+  })
+);
 
 // express middleware config.
-app.use( express.json( {limit : "16kb"} ) );
+app.use(express.json({ limit: "16kb" }));
 
 //URL Encoded Data (for form submissions)
-app.use( express.urlencoded( {extended : true , limit : "16kb"} ) );
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 //Static Files (Serve Images, CSS, JS)
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 
-app.use( cookieParser() );
-
+app.use(cookieParser());
 
 //after doing above config we will write routers.
 
 // routes
-import userRouter from "./routes/user.routes.js"
-
+import userRouter from "./routes/user.routes.js";
 
 // routes declaration   , we have to awake it like middleware.
-app.use( "/api/v1/users" ,  userRouter );
+app.use("/api/v1/users", userRouter);
 
 // http://localhost:3000/users/register , /users ke bad it will aytomatically append.
-
-
 
 export default app;
--- backend_project/src/services/cloudniary.js
+++ backend_project/src/services/cloudniary.js
@@ -1,30 +1,27 @@
-import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
 
-const uploadOnCloudinary  = async (localFilePath)=> {
-         try {
-          if(!localFilePath) return null;
+    //upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath);
 
-//upload file on cloudinary
-    const response =  await cloudinary.uploader.upload(localFilePath)
-          
-//file uploaded successfully
+    //file uploaded successfully
     console.log("file s uploaded successfully on cloudinary", response.url);
 
     return response;
-
-    } catch (error) {
-           fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation failed
-           return null;
-    }
-} 
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation failed
+    return null;
+  }
+};
 
 export { uploadOnCloudinary };
