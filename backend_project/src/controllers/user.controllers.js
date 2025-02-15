ff845ff59d7f4e00df6f56d5650503b17eee0700
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2025-02-15 05:29:00 +0000

    commit

--- backend_project/src/controllers/user.controllers.js
+++ backend_project/src/controllers/user.controllers.js
@@ -1,13 +1,35 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
 
+const registerUser = asyncHandler(async (req, res) => {
+  //1. get user details from frontend ;
+  //2. validation - check for empty
+  //3. check if user already existed - email, username
+  //4. check for file - coverimage , avatar
+  //5. upload to cloudinary.
+  //6. create a entry in DB -user{}
+  //7. remove password and refreshToken field from response.
+  //8. check for user creation
+  //9. return response.
 
-const registerUser =  asyncHandler( async (req , res) => {
-          res.status(200).json(
-            {
-              message : "ok"
-            }
-          )
-} )
+  //1.
+  const { username, fullName, email, password } = req.body;
 
+  console.log("email: ", email); //just to check
 
-export default registerUser ;
+  //2.
+  if (fullName === "") {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  //or
+  if (
+    [username, fullName, email, password].some(
+      (fields) => fields?.trim() === ""
+    )
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  //3.
+});
+
+export default registerUser;
--- backend_project/src/routes/user.routes.js
+++ backend_project/src/routes/user.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controllers.js"
+import registerUser from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
+const router = Router(); //like app from express
 
-const router = Router()  //like app from express
-
-router.route("/register").post(registerUser)
-
-
-
-
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
