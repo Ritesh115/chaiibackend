a4376147f5ca05bc40e833464f9b5da35189a427
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2025-02-26 09:22:12 +0000

    loginUser

--- backend_project/src/controllers/user.controllers.js
+++ backend_project/src/controllers/user.controllers.js
@@ -92,4 +92,84 @@ const registerUser = asyncHandler(async (req, res) => {
   return res.status(201).json({ createdUser: "user registered successfully" });
 });
 
-export default registerUser;
+
+// user login system
+
+const loginUser = asyncHandler(async (req, res) => {
+  //todos for login user
+
+  //1. get login data from frontend - req.body
+  //2. check through which input   , user is trying to login - email or username
+  //3. check if user exists in database
+  //4. check if password is correct
+  //5. create token for user - refresh and access token .
+  //6. send token to frontend - token is send in cookie .
+  //7. send response to frontend - user logged in successfully
+
+  //1
+  const { username, email, password } = req.body;
+
+  //2
+  if (!username || !email)
+    return res.status(400).json({ message: "Username or email is required" });
+
+  //3
+  const user = await User.findOne({
+    //user is instance of db user
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) return res.status(404).json({ message: "User does not exist" });
+
+  //4
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect)
+    return res.status(401).json({ message: "Password is incorrect" });
+
+  //5. import token functions from usermodels.
+  const accessToken = await user.generateAccessToken();
+  const refreshToken = await user.generateRefreshToken();
+
+  //now we need to save the refreshToken in the database.
+  user.refreshToken = refreshToken;
+  await user.save();
+
+ //remove password and refreshToken field from response.
+  const loggedInUser =  User.findById(user._id).select(  //this is for the response check .
+    "-password -refreshToken" 
+  ) ;
+
+  //6.   //7. //cookie options
+  const options = {
+    httpOnly: true ,
+    secure : true ,
+  }
+
+    return res
+    .status(200)
+    .cookie("accessToken" , accessToken , options)
+    .cookie("refreshToken" , refreshToken ,options)
+    .json(
+      {
+        User : loggedInUser , accessToken , refreshToken
+      },
+      'User logged in successfully'
+    )
+
+
+});
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+export { registerUser , loginUser };
