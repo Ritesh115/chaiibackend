38b421e7268507b13a274802c501d88e96885fd5
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2025-02-27 06:38:47 +0000

    commit

--- backend_project/src/middlewares/auth.middlewares.js
+++ backend_project/src/middlewares/auth.middlewares.js
@@ -9,27 +9,35 @@ const verifyjwt = asyncHandler( async (req , res , next)=>{
   //2. verify token and decode token
   //3. get user data from token and attach to req.user
 
-  //1
-    const token =  req.cookies?.accessToken || req.headers('Authorization') ?.replace('Bearer ' , '') ;
-
-    if(!token) return res.status(401).json({message: "Unauthorized: No token provided" });
-
-// if token is provided then we will verify token and get user data from token.
-//2.
-  const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET) ;
-
-//3. db req to get user data from token.
-  const user = await User.findById(decodedToken?._id).select(
-    "-password -refreshToken "
-  ) ; //_id is the access token payload.
+  try {
+    //1
+      const token =  req.cookies?.accessToken || req.headers('Authorization') ?.replace('Bearer ' , '') ;
   
+      if(!token) return res.status(401).json({message: "Unauthorized: No token provided" });
+  
+  // if token is provided then we will verify token and get user data from token.
+  //2.
+    const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET) ;
+  
+  //3. db req to get user data from token.
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken "
+    ) ; //_id is the access token payload.
+    
+  
+    if(!user) return res.status(401).json({message: "Unauthorized: No user found" });
+  
+    req.user = user ;  
+    next() ;
+  
+  } catch (error) {
+      return res.status(401).json(
+        error?.message || "Invalid Access token" 
+      );        
+  };
 
-  if(!user) return res.status(401).json({message: "Unauthorized: No user found" });
+});
 
-  req.user = user ;  
-  next() ;
-        
-} );
 
 
 export {verifyjwt} ;
